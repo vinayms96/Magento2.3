b12b739adbb9a9b661f651b04f95abb3adc0834c
vinayms96 <vinay.srikrishna@gmail.com>
2020-07-25 23:40:26 +0530

    Initial Framework

--- src/main/java/com/amazon/browser_setup/BrowserSetup.java
+++ src/main/java/com/amazon/browser_setup/BrowserSetup.java
@@ -1,12 +1,23 @@
 package com.amazon.browser_setup;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 
-public class BrowserSetup {
-    @BeforeClass
+import com.amazon.path.Constants;
+import com.amazon.utilities.Property;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BrowserSetup implements Constants {
+    public WebDriver driver = null;
+	    
+    @BeforeTest
     public void setup() {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setAcceptInsecureCerts(true);
@@ -17,7 +28,25 @@ public class BrowserSetup {
         FirefoxOptions ff_options = new FirefoxOptions();
         ff_options.merge(capabilities);
 
+        if (System.getProperty("head").equalsIgnoreCase("off")) {
+            ch_options.addArguments("--headless");
+            ff_options.addArguments("--headless");
+        }
 
-
+        if (System.getProperty("browser").equalsIgnoreCase("Chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (System.getProperty("browser").equalsIgnoreCase("Firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
+        driver.get(Property.get_Property("url"));
+        driver.manage().window().maximize();
     }
+
+    @AfterTest
+    public void finish() {
+        driver.quit();
+    }
+
 }
