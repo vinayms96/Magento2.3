509487bac9e1db93bf9ec82b8a06b1019286819b
vinayms96 <vinay.srikrishna@gmail.com>
2020-08-16 18:50:10 +0530

    Created a module to fetch the data from Database

---
+++ src/main/java/com/magento/utilities/DatabaseUtils.java
@@ -0,0 +1,65 @@
+package com.magento.utilities;
+
+import com.magento.loggers.Loggers;
+import com.magento.mysql.JdbcConnection;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+
+public class DatabaseUtils {
+    private static Connection connect = JdbcConnection.getConnection();
+    private static Statement stmt;
+    private static ResultSet result;
+    private static ResultSetMetaData resultSetMetaData;
+
+    /**
+     * Execute the query passed and return the ResultSet
+     *
+     * @param getQuery
+     * @return ResultSet
+     */
+    public static ResultSet getData(String getQuery) {
+        try {
+            /*Creating the Statement*/
+            stmt = connect.createStatement();
+            String query = getQuery;
+
+            /*Executing the query passed*/
+            result = stmt.executeQuery(query);
+
+            /*Moving the Position of the Cursor to first row*/
+            result.next();
+
+            /*Returning the ResultSet*/
+            return result;
+
+        } catch (Exception e) {
+            /*Logging any error occurred*/
+            Loggers.getLogger().error(e.getStackTrace()[0] + ", " + e.getStackTrace()[1]);
+        }
+
+        return null;
+    }
+
+    /**
+     * Getting the ResultSet Metadata for column count
+     * @return ResultSetMetaData
+     */
+    public static ResultSetMetaData getResultSetMetaData() {
+        try {
+            /*Fetching the MetaData*/
+            resultSetMetaData = result.getMetaData();
+
+            /*Returning the resultSetMetaData*/
+            return resultSetMetaData;
+
+        } catch (Exception e) {
+            /*Logging any error occurred*/
+            Loggers.getLogger().error(e.getStackTrace()[0] + ", " + e.getStackTrace()[1]);
+        }
+
+        return null;
+    }
+}
--- src/test/java/com/magento/AppTest.java
+++ src/test/java/com/magento/AppTest.java
@@ -2,9 +2,13 @@ package com.magento;
 
 import com.magento.extent_reports.ExtentReport;
 import com.magento.loggers.Loggers;
+import com.magento.mysql.JdbcConnection;
 import com.magento.path.Constants;
+import com.magento.utilities.DatabaseUtils;
 import com.magento.utilities.ExcelUtils;
 
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
 /**
@@ -22,18 +26,24 @@ public class AppTest implements Constants {
         System.out.println(ExcelUtils.getLastCellNumber());
         System.out.println(ExcelUtils.getDataMap().get("password"));
         Loggers.getLogger().info("Test");
-        ExtentReport.extentReport();
-        ExtentReport.createTest("New Test");
-        ExtentReport.createNode("Node11");
-        ExtentReport.getExtentNode().pass("Node1 Pass");
-        ExtentReport.createNode("Node12");
-        ExtentReport.getExtentNode().fail("Node2 Fail");
-        ExtentReport.getExtentTest().pass("Extent test Pass");
-        ExtentReport.getExtentReports().flush();
-//        JdbcConnection.establishConnection();
+//        ExtentReport.extentReport();
+//        ExtentReport.createTest("New Test");
+//        ExtentReport.createNode("Node11");
+//        ExtentReport.getExtentNode().pass("Node1 Pass");
+//        ExtentReport.createNode("Node12");
+//        ExtentReport.getExtentNode().fail("Node2 Fail");
+//        ExtentReport.getExtentTest().pass("Extent test Pass");
+//        ExtentReport.getExtentReports().flush();
+        JdbcConnection.establishConnection();
 //        if(JdbcConnection.getConnection() != null){
 //            System.out.println(JdbcConnection.getConnection().getClientInfo());
 //        }
+        ResultSet result = DatabaseUtils.getData("SELECT * FROM customer_data WHERE id=1;");
+        System.out.println(result);
+        for (int i = 1; i <= DatabaseUtils.getResultSetMetaData().getColumnCount(); i++) {
+            System.out.println(result.getString(i));
+        }
+//        System.out.println(result.getString(0));
 //        DynamicPath.getScreenshotPath();
 //        System.out.println(Screenshot.getScreenshotBase64());
         System.out.println("Completed");
